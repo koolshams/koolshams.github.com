c9f0eda1a316b7210df7718236c755cfa6dd371e
Shamseer Srambikool <shamseerss@gmail.com>
2016-08-04 09:51:42 -0400

    Update page.js

--- chart-demo/scripts/page.js
+++ chart-demo/scripts/page.js
@@ -11,11 +11,11 @@ $(function () {
 
 
         title: {
-            text: 'Sales per employee per weekday'
+            text: 'Data Access per Virtual Device per weekday'
         },
 
         xAxis: {
-            categories: ['Alexander', 'Marie', 'Maximilian', 'Sophia', 'Lukas', 'Maria', 'Leon', 'Anna', 'Tim', 'Laura']
+            categories: ['Oracle-Emp', 'Finance-Tbl', 'Projections', 'LUN2', 'LUN3', 'Oracle-Log', 'Sybase-DB1', 'Finance-Main', 'LUN10', 'LUN-42']
         },
 
         yAxis: {
@@ -44,13 +44,13 @@ $(function () {
 
         tooltip: {
             formatter: function () {
-                return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> sold <br><b>' +
-                    this.point.value + '</b> items on <br><b>' + this.series.yAxis.categories[this.point.y] + '</b>';
+                return '<b>' + this.series.xAxis.categories[this.point.x] + '</b> Data Accessed <br><b>' +
+                    this.point.value + '</b> K on <br><b>' + this.series.yAxis.categories[this.point.y] + '</b>';
             }
         },
 
         series: [{
-            name: 'Sales per employee',
+            name: 'Data Access per Virtual Device',
             borderWidth: 1,
             data: [[0, 0, 10], [0, 1, 19], [0, 2, 8], [0, 3, 24], [0, 4, 67], [1, 0, 92], [1, 1, 58], [1, 2, 78], [1, 3, 117], [1, 4, 48], [2, 0, 35], [2, 1, 15], [2, 2, 123], [2, 3, 64], [2, 4, 52], [3, 0, 72], [3, 1, 132], [3, 2, 114], [3, 3, 19], [3, 4, 16], [4, 0, 38], [4, 1, 5], [4, 2, 8], [4, 3, 117], [4, 4, 115], [5, 0, 88], [5, 1, 32], [5, 2, 12], [5, 3, 6], [5, 4, 120], [6, 0, 13], [6, 1, 44], [6, 2, 88], [6, 3, 98], [6, 4, 96], [7, 0, 31], [7, 1, 1], [7, 2, 82], [7, 3, 32], [7, 4, 30], [8, 0, 85], [8, 1, 97], [8, 2, 123], [8, 3, 64], [8, 4, 84], [9, 0, 47], [9, 1, 114], [9, 2, 31], [9, 3, 48], [9, 4, 91]],
             dataLabels: {
@@ -79,12 +79,12 @@ $(function () {
 
 
         title: {
-            text: 'Highcharts heat map',
+            text: 'Data Heatmap',
             align: 'left'
         },
 
         subtitle: {
-            text: 'Temperature variation by day and hour through May 2015',
+            text: 'Data Access variation by day and hour through May 2015',
             align: 'left'
         },
 
@@ -124,8 +124,8 @@ $(function () {
             borderWidth: 0,
             colsize: 24 * 36e5, // one day
             tooltip: {
-                headerFormat: 'Temperature<br/>',
-                pointFormat: '{point.x:%e %b, %Y} {point.y}:00: <b>{point.value} ℃</b>'
+                headerFormat: 'Data Access<br/>',
+                pointFormat: '{point.x:%e %b, %Y} {point.y}:00: <b>{point.value} K</b>'
             }
         }]
 
